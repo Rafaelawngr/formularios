c678dbdd52b427d00de19379fc9276b14d03e8e9
Rafaela Ellwanger <107516309+Rafaelawngr@users.noreply.github.com>
2023-06-28 23:13:35 -0300

    add 2 page

--- script.js
+++ script.js
@@ -1,76 +1,44 @@
 const form = document.querySelector("#login");
-form.addEventListener('submit', submitForm)
-
+form.addEventListener("submit", submitForm);
 
 function submitForm(e) {
-    e.preventDefault()
-  
-    const formValues = Object.fromEntries(new FormData(e.target))
-    
-    let usuarioValidado = false
+  e.preventDefault();
 
-    for (let i = 0; i < listaUsers.length; i++) {
-        if (formValues.username === listaUsers[i].username && formValues.password === listaUsers[i].password) {
-            usuarioValidado = true
-        }   
+  const formValues = Object.fromEntries(new FormData(e.target));
+
+  let usuarioValidado = false;
+  const errorMessage = document.getElementById("error-message");
+
+  for (let i = 0; i < listaUsers.length; i++) {
+    if (
+      formValues.username === listaUsers[i].username &&
+      formValues.password === listaUsers[i].password
+    ) {
+      usuarioValidado = true;
     }
-
-    if (usuarioValidado == true) {
-        // clica no botao e direciona para a outra pagina
-    } else {
-        console.warn('nao encontrado')
-    }   
   }
-  
-class User {
-    username = "";
-    password = "";
 
-    constructor(usuario, senha) {
-        this.username = usuario;
-        this.password = senha
-    }
+  if (usuarioValidado == true) {
+    window.location.href = "page2.html";
+  } else {
+    errorMessage.innerHTML = "Ops, parece que você não está cadastrado!";
+  }
 }
 
-const user1 = new User('Rafaela', '159753');
-const user2 = new User('Jorge', '951357');
-const user3 = new User('Brenda', '753951');
+class User {
+  username = "";
+  password = "";
+
+  constructor(usuario, senha) {
+    this.username = usuario;
+    this.password = senha;
+  }
+}
+
+const user1 = new User("Rafaela", "159753");
+const user2 = new User("Jorge", "951357");
+const user3 = new User("Brenda", "753951");
 
 const listaUsers = [user1, user2, user3];
 
-
-// se o usuário que realizar o login estiver cadastrado - redireciona para a página 2
-// se não estiver, mensagem de erro no html
-
-
-
-
-
-// mensagem de erro leva para o botao de criar conta e aparece uma 2a pagina, contendo NOME, SOBRENOME, IMAGEM DE PERFIL, ENDEREÇO, DATA DE NASCIMENTO, POSSUI FILHOS? SE SIM, QUANTOS. ESPORTE FAVORITO etc.
-// inputs: text, radio, checkbox, select option,etc
-// Botão confirmar: todos os dados inseridos de forma organizada (estilo página de perfil)
-
-
-
-
-// function validateForm() {
-
-// }
-
-// function checkInputs() {
-//     const fullNameValue = fullName.value
-//     const passwordValue = password.value
-
-//     if (fullNameValue === '') {
-//         // mostrar erro
-//     } else {
-//         // mostrar classe sucesso
-//     }
-
-//     if (passwordValue === '') {
-//         // mostrar erro
-//     } else {
-//         // mostrar classe sucesso
-//     }
-// }
 
